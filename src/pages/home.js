83daaadead61a89f75e4f71f13dee152789d6c98
amora7741 <mora7741@gmail.com>
2024-01-11 16:18:31 -0800

    Complete home tab

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,7 +1,23 @@
+import chef from "../images/chef.jpeg";
+
 export default function renderHome() {
   const element = document.createElement("div");
 
   element.classList.add("boxinfo");
+  const title = document.createElement("h2");
+  title.textContent = "Welcome to B.R.Z!";
+  const info = document.createElement("p");
+  info.textContent = "Open 24/7 with the Finest Cuisine";
+  const chefRamsey = new Image();
+  const description = document.createElement("p");
+  description.textContent = "Meet our lead chef: Gordon Ramsey!";
+
+  chefRamsey.src = chef;
+
+  element.appendChild(title);
+  element.appendChild(info);
+  element.appendChild(chefRamsey);
+  element.appendChild(description);
 
   return element;
 }
