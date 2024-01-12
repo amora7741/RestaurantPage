6e0447d5d21e47aea8fc1973c5dac1c6c1f4856f
amora7741 <mora7741@gmail.com>
2024-01-12 12:38:43 -0800

    Adjust item container

--- src/components/menuitem.js
+++ src/components/menuitem.js
@@ -2,14 +2,14 @@ export default function createMenuItem(name, img) {
   const menuItem = document.createElement("div");
   menuItem.classList.add("itemcontainer");
 
-  const info = document.createElement("h2");
+  const info = document.createElement("p");
   info.textContent = name;
 
   const pic = new Image();
   pic.src = img;
 
-  menuItem.appendChild(info);
   menuItem.appendChild(pic);
+  menuItem.appendChild(info);
 
   return menuItem;
 }
